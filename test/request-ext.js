861852b316421c532d53c5f28ff78093fff8b261
Boris Okunskiy <i@savant.pro>
2014-03-10 22:56:40 +0400

    Added getNumber, getNumbers.

--- test/request-ext.js
+++ test/request-ext.js
@@ -128,9 +128,9 @@ describe('Request extensions', function() {
     });
 
     it('returns a parsed integer', function(cb) {
-      request.get('http://localhost:8123/?foo=10', function(err, res, body) {
+      request.get('http://localhost:8123/?foo=3.14', function(err, res, body) {
         if (err) return cb(err);
-        assert.equal(body, 'foo=10');
+        assert.equal(body, 'foo=3');
         cb();
       });
     });
@@ -169,6 +169,71 @@ describe('Request extensions', function() {
       request.get('http://localhost:8123/?foo=10&foo=abc&foo=2', function(err, res, body) {
         if (err) return cb(err);
         assert.equal(body, '11, 3');
+        cb();
+      });
+    });
+
+  });
+
+  describe('getNumber', function() {
+
+    var app = new Application();
+    var server = http.createServer(app);
+
+    before(function(cb) {
+      server.listen(app.conf.port, cb);
+    });
+
+    after(function(cb) {
+      server.close(cb);
+    });
+
+    app.get('/', function(req, res, next) {
+      res.send('foo=' + req.getNumber('foo', -1));
+    });
+
+    it('returns a parsed number', function(cb) {
+      request.get('http://localhost:8123/?foo=3.14', function(err, res, body) {
+        if (err) return cb(err);
+        assert.equal(body, 'foo=3.14');
+        cb();
+      });
+    });
+
+    it('returns a default value if parameter unspecified', function(cb) {
+      request.get('http://localhost:8123/', function(err, res, body) {
+        if (err) return cb(err);
+        assert.equal(body, 'foo=-1');
+        cb();
+      });
+    });
+
+  });
+
+  describe('getNumbers', function() {
+
+    var app = new Application();
+    var server = http.createServer(app);
+
+    before(function(cb) {
+      server.listen(app.conf.port, cb);
+    });
+
+    after(function(cb) {
+      server.close(cb);
+    });
+
+    app.get('/', function(req, res, next) {
+      res.send(
+        req.getNumbers('foo')
+          .map(function(n) { return n + 1 })
+          .join(', '));
+    });
+
+    it('parses an array of integers', function(cb) {
+      request.get('http://localhost:8123/?foo=10.3&foo=abc&foo=2.3', function(err, res, body) {
+        if (err) return cb(err);
+        assert.equal(body, '11.3, 3.3');
         cb();
       });
     });
